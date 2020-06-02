cfb1491c7029f32f81dbfcb88177a2d5f4874b7d
Dell <user@dell.com>
2020-06-02 14:09:04 -0500

    Nested menu and click event 1

--- script/script.js
+++ script/script.js
@@ -7,4 +7,16 @@ function init(){
     btn.onclick = () => {
         list.classList.toggle('active');
     }
+   
+    const menu = document.querySelector('nav > ul');
+
+    menu.addEventListener('click', e => {        
+        let smenu = e.target.parentNode;
+
+        if(smenu.childNodes[1]){
+            smenu.classList.toggle('active');
+        }
+
+        console.log(smenu.className);
+    })
 }
