fff6a815baee6999c100b8531f0286858b16dbb0
Dell <user@dell.com>
2020-06-02 11:44:42 -0500

    Click event and final styles

--- script/script.js
+++ script/script.js
@@ -1,5 +1,10 @@
 document.addEventListener('DOMContentLoaded', init);
 
 function init(){
-    
+    const btn = document.querySelector('.menu');
+    const list = document.querySelector('#list');
+
+    btn.onclick = () => {
+        list.classList.toggle('active');
+    }
 }
